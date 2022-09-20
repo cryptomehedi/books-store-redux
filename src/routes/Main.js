76f4825ab7ece79c646c426eee2dce1b937ddf01
cryptomehedi <classicalmehedi@gmail.com>
2022-09-21 00:15:12 +0600

    routes

---
+++ src/routes/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import AddBook from '../Features/Books/AddBook';
+import BooksView from '../Features/Books/BooksView';
+import Footer from '../Layouts/Footer';
+import Nav from '../Layouts/Nav';
+import Error from '../Pages/Error';
+import Home from '../Pages/Home';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav/>
+            <main>
+                <Routes>
+                    <Route path="/home" element={<Home/>} />
+                    <Route path="/show-books" element={<BooksView/>} />
+                    <Route path="/add-book" element={<AddBook/>} />
+                    <Route path="*" element={<Error/>} />
+                </Routes>
+            </main>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
