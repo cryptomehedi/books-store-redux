7e76194c781ae4ec5a3276511159c2eeee71c73e
cryptomehedi <classicalmehedi@gmail.com>
2022-09-21 00:17:09 +0600

    pages

---
+++ src/Pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Home';
+
+const Error = () => {
+    return (
+        <div>
+            <Home/>
+        </div>
+    );
+};
+
+export default Error;
